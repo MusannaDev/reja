e3c8a5eb744814062a61170fa5c7930005a87778
MusannaDev <juraevotabek2002@gmail.com>
2025-09-17 00:21:32 +0900

    xarid.ejs fayli ishga tushirildi

--- server.js
+++ server.js
@@ -14,13 +14,14 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routers code 
-app.get("/", function (req, res) {
-    res.end("<h1>HELLO WORLD bu Otabek(Adam)</h1>");
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "success"});
 });
 
-app.get("/gift", function (req, res) {
-    res.end("<h1>Siz sovg'alar bo'limidasiz</h1>");
-});
+app.get("/", function (req, res) {
+    res.render("xarid");
+})
 
 const server = http.createServer(app);
 let PORT = 3000;
