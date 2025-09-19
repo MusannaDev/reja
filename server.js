515f8510656ec1689203db127abe221cafb33a8f
MusannaDev <juraevotabek2002@gmail.com>
2025-09-19 19:12:13 +0900

    feat: author ejs is running

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 console.log("Web serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
 const fs = require("fs");
 
+
 let user;
-fs.readFile("database/user,json", "utf8", (err, data) => {
+fs.readFile("database/user.json", "utf8", (err, data) => {
     if (err) {
-        console.log("ERROR:", err);
+        console.log("ERROR:", err);  
     } else {
         user = JSON.parse(data);
     }
-})
+});
 
 // 1 Kirish code
 app.use(express.static("public"));
@@ -24,14 +26,14 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routers code 
-app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test: "success"});
-});
+//app.post("/create-item", (req, res) => {
+    // console.log(req.body);
+    // res.json({test: "success"});
+//});
 
-app.get("/author", (req, res) => {
-    res.render("author", {user:user});
-});
+app.get('/author', (req, res) => {
+     res.render("author", {user:user});
+ }); 
 
 app.get("/", function (req, res) {
     res.render("xarid");
@@ -42,3 +44,4 @@ let PORT = 3000;
 server.listen(PORT, function () {
     console.log(`The server is running successfully on port: ${PORT}`);
 });
+
